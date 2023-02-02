6898547d0efdf9f27690fc0b2ceac1f206eeafe7
Reddysekhar Gaduputi <gsekhar73@gmail.com>
2023-02-02 15:22:27 +0530

    Update cloud-init.sh

--- cloud-init.sh
+++ cloud-init.sh
@@ -24,7 +24,7 @@ echo "alias k=kubectl" | sudo tee /home/adminuser/.bash_aliases
 # docker
 sudo apt-get install ca-certificates curl gnupg lsb-release -y
 sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor --yes -o /etc/apt/keyrings/docker.gpg
 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
