4ac1ff2e1eb42da28756b1f35892d9c47d16fbcd
Reddysekhar Gaduputi <gsekhar73@gmail.com>
2023-02-02 15:16:43 +0530

    Update cloud-init.sh

--- cloud-init.sh
+++ cloud-init.sh
@@ -18,24 +18,24 @@ curl -sL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 |
 # kubectl
 curl -LO "https://dl.k8s.io/release/v1.23.12/bin/linux/amd64/kubectl"
 chmod +x kubectl
-mv -f kubectl /usr/bin/
+sudo mv -f kubectl /usr/bin/
 echo "alias k=kubectl" | sudo tee /home/adminuser/.bash_aliases
 
 # docker
-#sudo apt-get install ca-certificates curl gnupg lsb-release -y
-#sudo mkdir -p /etc/apt/keyrings
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo apt-get install ca-certificates curl gnupg lsb-release -y
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
-#echo \
-#  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-#  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-#sudo apt-get update -y
+sudo apt-get update -y
 
-#sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh ./get-docker.sh
+#curl -fsSL https://get.docker.com -o get-docker.sh
+#sudo sh ./get-docker.sh
 
 # set kubectl, helm bash completion
 echo "source <(helm completion bash)" >> /home/adminuser/.bashrc
@@ -44,5 +44,5 @@ echo "source <(kubectl completion bash)" >> /home/adminuser/.bashrc
 echo "" >> /home/adminuser/.bashrc
 
 
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh ./get-docker.sh
+#curl -fsSL https://get.docker.com -o get-docker.sh
+#sudo sh ./get-docker.sh
